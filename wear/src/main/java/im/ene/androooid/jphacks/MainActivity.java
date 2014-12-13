4b5d90b5c48b7868456ce71a59bc8f8508110b68
TETRA2000 <sagittariusm25@gmail.com>
2014-12-13 17:09:01 +0900

    heart rate!!

--- wear/src/main/java/im/ene/androooid/jphacks/MainActivity.java
+++ wear/src/main/java/im/ene/androooid/jphacks/MainActivity.java
@@ -1,17 +1,28 @@
 package im.ene.androooid.jphacks;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.view.WindowManager;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements SensorEventListener {
     private TextView mTextView;
+
+    private SensorManager mSensorManager;
+    private Sensor mHeartRateSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // keep screen on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         setContentView(R.layout.activity_main);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
@@ -20,5 +31,42 @@ public class MainActivity extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
+
+        mSensorManager = ((SensorManager)getSystemService(SENSOR_SERVICE));
+        mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        mSensorManager.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        mSensorManager.unregisterListener(this, mHeartRateSensor);
+
+        super.onPause();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (mTextView==null) return; // not loaded yet
+
+        float heartRate = event.values[0];
+
+        if (heartRate < 1.0) // too low (may be an error)
+            return;
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("heart rate:");
+        builder.append(event.values[0]);
+        mTextView.setText(builder.toString());
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
